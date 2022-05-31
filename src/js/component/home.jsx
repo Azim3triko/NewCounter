d8a229accd5e3c9f6c557eb0d91c65107cf2eba3
Azim3triko <azim3triko@protonmail.com>
2022-05-31 17:18:38 +0000

    otra version de counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,49 +1,47 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import PropTypes from "prop-types";
 
 //include images into your bundle
 
 //create your first component
 
-export const Home = () => {
-	return (
-		<React.Fragment>
-			<Counter />
-		</React.Fragment>
-	);
-};
+//  export Home = () => {
+//  	return (
+//  		<React.Fragment>
+// 			<NewCounter />
+// 		</React.Fragment>
+// 	);
+//  };
 
-export function Counter(props) {
-	return (
-		<div className="Container d-flex bg-dark">
-			<div className="align-content-center col-3 fs-1" id="Clock">
-				<p className="text-light text-center">
-					<i className="fas fa-clock"></i>
-				</p>
-			</div>
-			<div className="justify-content-center col-2 fs-1">
-				<p className="text-light">{props.numberFive}</p>
-			</div>
-			<div className="justify-content-center col-2 fs-1">
-				<p className="text-light">{props.numberFour}</p>
-			</div>
-			<div className="justify-content-center col-2 fs-1">
-				<p className="text-light">{props.numberThree}</p>
-			</div>
-			<div className="justify-content-center col-2 fs-1">
-				<p className="text-light">{props.numberTwo}</p>
-			</div>
-			<div className="justify-content-center col-2 fs-1">
-				<p className="text-light">{props.numberOne}</p>
-			</div>
-		</div>
-	);
-}
+// function NewCounter(props) {
+// 	return (
+//  		<div className="Container d-flex bg-dark">
+// 			<div className="align-content-center col-3 fs-1" id="Clock">
+// 			<p className="text-light text-center">
+// 				<i className="fas fa-clock"></i>
+// 			</p>
+// 		</div>
+// 			<p className="text-light">{props.numberFive}</p>
+// 		</div>
+// 		<p className="text-light">{props.numberFour}</p>
+// 		</div >
+// 			<div className="justify-content-center col-2 fs-1">
+// 			<p className="text-light">{props.numberThree}</p>
+// 		</div>
+// 		<div className="justify-content-center col-2 fs-1">
+// 			<p className="text-light">{props.numberTwo}</p>
+// 		</div>
+// 	    <div className="justify-content-center col-2 fs-1">
+// 			<p className="text-light">{props.numberOne}</p>
+//  			</div>
+//  		</div >
+// 	);
+// }
 
-Counter.propTypes = {
-	numberFive: PropTypes.number,
-	numberFour: PropTypes.number,
-	numberThree: PropTypes.number,
-	numberTwo: PropTypes.number,
-	numberOne: PropTypes.number,
-};
+// // Counter.propTypes = {
+// // 	numberFive: PropTypes.number,
+// // 	numberFour: PropTypes.number,
+// // 	numberThree: PropTypes.number,
+// // 	numberTwo: PropTypes.number,
+// // 	numberOne: PropTypes.number,
+// // };
--- src/js/index.js
+++ src/js/index.js
@@ -4,27 +4,17 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 
-import Home, { Counter } from "./component/home.jsx";
+import { App } from "./component/App";
 
-let seconds = 0;
-setInterval(() => {
-	let segFive = Math.floor(seconds / 10000);
-	let segFour = Math.floor(seconds / 1000);
-	let segThree = Math.floor(seconds / 100);
-	let segTwo = Math.floor(seconds / 10);
-	let segOne = Math.floor(seconds / 1);
-	seconds++;
-	console.log(segFive, segFour, segThree, segTwo, segOne);
-	ReactDOM.render(
-		<Counter
-			numberOne={segOne}
-			numberTwo={segTwo}
-			numberThree={segThree}
-			numberFour={segFour}
-			numberFive={segFive}
-		/>,
-		document.querySelector("#app")
-	);
-}, 1000);
+//  let seconds = 0;
+//  setInterval(() => {
+// 	let segFive = Math.floor(seconds / 10000);
+// 	let segFour = Math.floor(seconds / 1000);
+// let segThree = Math.floor(seconds / 100);
+// 	let segTwo = Math.floor(seconds / 10);
+//  	let segOne = Math.floor(seconds / 1);
+// 	seconds++;
+//  	console.log(segFive, segFour, segThree, segTwo, segOne);
+ReactDOM.render(<App />, document.querySelector("#app"));
 
 //render your react application
